906641ebf629beac6883487bd1ccd00d02572203
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-02-18 19:00:59 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
+
+  const formLink = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
+
   return (
     <div className="App">
-      <header>
-  
-      </header>
+      <Header />
+  <div id='our-forms'>
+    
+    <h2>TELL US HOW WE CAN HELP YOU:</h2>
+
+    <div id='link-box'>
+
+    <div className='link-row'>
+   <div className='link-descrip'>
+    <p className='descrip'>"Do you have an Emergency? No Problem,<br/> You're in the right place. <br/>This is the quickest way to get our help!<br/> NO EXTRA FEE'S" </p>
+    {/* <p className='descrip'> This is the quickest way to get our help! </p>
+    <p className='descrip'>NO EXTRA FEE'S"</p> */}
+  </div>
+    <button onClick={() => formLink('https://airtable.com/shrl3qG3vHipQyF4z')} class='button-link' style={{color:'blue'}}><u>EMERGENCY</u></button>
+    </div>
+
+    <div className='link-row'>
+    <div className='link-descrip'>
+    <p className='descrip'>"This form is to provide us with<br/>all the info needed to give you a<br/> QUICK QUOTE for your plumbing need(s)." </p>
+    {/* <p className='descrip'>the info needed to give you a QUICK QUOTE </p>
+    <p className='descrip'>for your plumbing need(s)." </p>   */}
+    </div>
+    <button onClick={() => formLink('https://airtable.com/shrzS2zpB5ANgRHiO')} class='button-link' style={{color:'blue'}}><u>PLUMBING NON EMERGENCY</u></button>
+    </div>
+
+    <div className='link-row'>
+    <div className='link-descrip'>
+     <p className='descrip'>"This form is to provide us with <br/>all the info needed to give you a<br/>QUICK QUOTE for your HVAC need(s)." </p>  
+     {/* <p className='descrip'></p>  
+     <p className='descrip'></p>   */}
+    </div>
+    <button onClick={() => formLink('https://airtable.com/shrqifwbDY23d1nJx')} class='button-link' style={{color:'blue'}}><u>HVAC NON EMERGENCY</u></button>
+    </div>
+   
+
+  </div>
+
+</div>
+
+    
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+
+
+
+  return (
+    <header>
+        <div className='header-side'>
+
+        </div>
+
+        <div className='header-center'>
+            <img src='/quick-quote-banner.png' className='banner-logo'></img>
+        </div>
+
+        <div className='header-side'>
+
+</div>
+
+
+    </header>
+  )
+}
+
+export default Header
