250ae63dc6cdace551a71715be7b33abaf2394bd
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-02-18 19:21:38 -0500

    embbed form / responesive desing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+    
+        <iframe className="airtable-embed" src="https://airtable.com/embed/shrzS2zpB5ANgRHiO?backgroundColor=red" >
+        </iframe>
+   
+
+
   <div id='our-forms'>
     
     <h2>TELL US HOW WE CAN HELP YOU:</h2>
@@ -20,8 +27,7 @@ function App() {
     <div className='link-row'>
    <div className='link-descrip'>
     <p className='descrip'>"Do you have an Emergency? No Problem,<br/> You're in the right place. <br/>This is the quickest way to get our help!<br/> NO EXTRA FEE'S" </p>
-    {/* <p className='descrip'> This is the quickest way to get our help! </p>
-    <p className='descrip'>NO EXTRA FEE'S"</p> */}
+  
   </div>
     <button onClick={() => formLink('https://airtable.com/shrl3qG3vHipQyF4z')} class='button-link' style={{color:'blue'}}><u>EMERGENCY</u></button>
     </div>
@@ -29,8 +35,7 @@ function App() {
     <div className='link-row'>
     <div className='link-descrip'>
     <p className='descrip'>"This form is to provide us with<br/>all the info needed to give you a<br/> QUICK QUOTE for your plumbing need(s)." </p>
-    {/* <p className='descrip'>the info needed to give you a QUICK QUOTE </p>
-    <p className='descrip'>for your plumbing need(s)." </p>   */}
+
     </div>
     <button onClick={() => formLink('https://airtable.com/shrzS2zpB5ANgRHiO')} class='button-link' style={{color:'blue'}}><u>PLUMBING NON EMERGENCY</u></button>
     </div>
@@ -38,8 +43,7 @@ function App() {
     <div className='link-row'>
     <div className='link-descrip'>
      <p className='descrip'>"This form is to provide us with <br/>all the info needed to give you a<br/>QUICK QUOTE for your HVAC need(s)." </p>  
-     {/* <p className='descrip'></p>  
-     <p className='descrip'></p>   */}
+   
     </div>
     <button onClick={() => formLink('https://airtable.com/shrqifwbDY23d1nJx')} class='button-link' style={{color:'blue'}}><u>HVAC NON EMERGENCY</u></button>
     </div>
